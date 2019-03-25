26be3216769673a9e45f5a17b82ebe92962e5674
dorama2017 <1035073267@qq.com>
2019-03-25 23:35:13 +0800

    page53

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Post
+
+
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','slug','pub_date')
 
 # Register your models here.
+admin.site.register(Post,PostAdmin)
